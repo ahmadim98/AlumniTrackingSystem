c5d9051968bf34c9188b78aa417da0dd272f2c47
ahmadim98 <iahmadbinibrahim@gmail.com>
2020-02-14 17:47:17 +0300

    added new function to logout + applying some detailed design diagram based on our report

---
+++ lib/question.dart
@@ -0,0 +1,23 @@
+class Question {
+  List<String> Options;
+  String QuestionTitle;
+
+  /*ser(int username,int password,bool loggedin){
+    this.username = null;
+    this.password = null;
+    this.loggedin = false;
+  }*/
+
+}
+
+class Survey {
+  List<Question> Questions;
+  int Date;
+
+/*ser(int username,int password,bool loggedin){
+    this.username = null;
+    this.password = null;
+    this.loggedin = false;
+  }*/
+
+}
