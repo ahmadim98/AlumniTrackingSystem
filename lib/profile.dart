de0d05e48e759c22687f3806dddb72147ccd8b99
AbdulrahmanBinShmlan <38705497+AbdulrahmanBinShmlan@users.noreply.github.com>
2020-02-23 00:21:57 +0300

    01
    chat,profile classes

---
+++ lib/profile.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+//import 'package:alumniapp/const.dart';
+
+class profile extends StatefulWidget {
+  @override
+  _profileState createState() => _profileState();
+}
+
+class _profileState extends State<profile> {
+  final TextEditingController textEditingController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+
+
+        title: Text('Profile'),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/user picture.png'),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 50,
+              color: Colors.grey[600],
+            ),
+            Text(
+              'Name',
+              style: TextStyle(
+                fontSize: 18,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              'Abdulrhman Bin Shmlan',
+              style: TextStyle(
+                  fontSize: 20,
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30,),
+            Text(
+              'Major',
+              style: TextStyle(
+                fontSize: 18,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              'Software Engineering',
+              style: TextStyle(
+                  fontSize: 20,
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30,),
+            Row(
+              children: <Widget>[
+                Icon(
+                    Icons.phone
+                ),
+                SizedBox(width: 10,),
+                Text(
+                  '0534671851',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
